da12bfbd4953e3b2da1a944deaff5737e6275b32
Gustavo Honorato <gustavohonorato@gmail.com>
2024-12-27 16:48:33 -0300

    add initial magic auth components generation

--- lib/mix/tasks/magic_auth.install.ex
+++ lib/mix/tasks/magic_auth.install.ex
@@ -4,17 +4,29 @@ defmodule Mix.Tasks.MagicAuth.Install do
 
   @shortdoc "Installs Magic Auth"
 
-  @template_dir "priv/templates/magic_auth.install"
+  @template_dir "#{:code.priv_dir(:magic_auth)}/templates/magic_auth.install"
 
-  def run(_args) do
-    assigns = %{}
-    install(assigns)
+  def run(args) do
+    args
+    |> build_assigns()
+    |> install()
+  end
+
+  defp build_assigns(_args) do
+    %{
+      app_name: Mix.Phoenix.otp_app(),
+      base_module: Mix.Phoenix.base(),
+      web_module:
+        Mix.Phoenix.base() |> Mix.Phoenix.web_module() |> Atom.to_string() |> String.replace_prefix("Elixir.", "")
+    }
   end
 
   defp install(assigns) do
     install_magic_token_migration_file(assigns)
+    install_magic_auth_components(assigns)
 
     Mix.shell().info("""
+
     Magic Auth installed successfully!
 
     Don't forget to run the migration:
@@ -23,14 +35,39 @@ defmodule Mix.Tasks.MagicAuth.Install do
   end
 
   defp install_magic_token_migration_file(assigns) do
-    timestamp = NaiveDateTime.utc_now() |> NaiveDateTime.to_string() |> String.replace(["-", ":", " "], "")
-
-    create_directory("priv/repo/migrations")
-
     copy_template(
       "#{@template_dir}/create_magic_auth_tokens.exs.eex",
-      "priv/repo/migrations/#{timestamp}_create_magic_auth_tokens.exs",
+      "priv/repo/migrations/#{generate_migration_timestamp()}_create_magic_auth_tokens.exs",
       assigns
     )
+  end
+
+  defp generate_migration_timestamp do
+    NaiveDateTime.utc_now()
+    |> NaiveDateTime.truncate(:second)
+    |> NaiveDateTime.to_string()
+    |> String.replace(["-", ":", " "], "")
+  end
+
+  defp install_magic_auth_components(assigns) do
+    components_dir = "lib/#{assigns.app_name}_web/components"
+
+    copy_template(
+      "#{@template_dir}/magic_auth_components.ex.eex",
+      "#{components_dir}/magic_auth_components.ex",
+      assigns
+    )
+
+    config_file = "config/config.exs"
+    Mix.shell().info(IO.ANSI.cyan() <> "* injecting " <> IO.ANSI.reset() <> "#{config_file}")
+
+    config_content = File.read!(config_file)
+
+    unless String.match?(config_content, ~r/config :magic_auth\b/) do
+      File.write!(
+        config_file,
+        config_content <> "\n\nconfig :magic_auth, ui_components: #{assigns.web_module}.MagicAuthComponents"
+      )
+    end
   end
 end
--- test/mix/tasks/magic_auth.install_test.exs
+++ test/mix/tasks/magic_auth.install_test.exs
@@ -5,35 +5,37 @@ defmodule Mix.Tasks.MagicAuth.InstallTest do
   import ExUnit.CaptureIO
 
   setup do
+    File.mkdir_p!("config")
+
+    File.write!("config/config.exs", """
+    use Mix.Config
+    """)
+
     on_exit(fn ->
       File.rm_rf!("priv/repo")
+      File.rm_rf!("lib/magic_auth/components")
+      File.rm_rf!("config/config.exs")
     end)
 
     :ok
   end
 
   test "creates migration file successfully" do
-    # Capture console output during execution
     output =
       capture_io(fn ->
         run(%{})
       end)
 
-    # Verify if migrations directory was created
     assert File.dir?("priv/repo/migrations")
 
-    # Find the created migration file
     [migration_file] = Path.wildcard("priv/repo/migrations/*_create_magic_auth_tokens.exs")
 
-    # Verify if file exists
     assert File.exists?(migration_file)
 
-    # Verify if file content contains expected data
     content = File.read!(migration_file)
     assert content =~ "defmodule Repo.Migrations.CreateMagicAuthTokens"
     assert content =~ "create table(:magic_auth_tokens)"
 
-    # Verify success message
     assert output =~ "Magic Auth installed successfully!"
   end
 
@@ -44,5 +46,56 @@ defmodule Mix.Tasks.MagicAuth.InstallTest do
       end)
 
     assert output =~ "Magic Auth installed successfully!"
+  end
+
+  test "creates magic auth components file" do
+    capture_io(fn ->
+      run([])
+    end)
+
+    components_file = "lib/magic_auth_web/components/magic_auth_components.ex"
+
+    assert File.exists?(components_file)
+
+    content = File.read!(components_file)
+    assert content =~ "defmodule MagicAuthWeb.MagicAuthComponents"
+    assert content =~ "use Phoenix.Component"
+  end
+
+  test "injects configuration into config.exs" do
+    capture_io(fn ->
+      run([])
+    end)
+
+    config_content = File.read!("config/config.exs")
+
+    assert config_content =~ "config :magic_auth, ui_components: MagicAuthWeb.MagicAuthComponents"
+  end
+
+  test "doesn't duplicate configuration if already present" do
+    File.rm("config/config.exs")
+
+    File.write!("config/config.exs", """
+    use Mix.Config
+    config :magic_auth, ui_components: MagicAuthWeb.MagicAuthComponents
+    """)
+
+    initial_content = File.read!("config/config.exs")
+
+    capture_io(fn ->
+      run([])
+    end)
+
+    final_content = File.read!("config/config.exs")
+
+    assert initial_content == final_content
+  end
+
+  test "raises if config file is not found" do
+    File.rm("config/config.exs")
+
+    assert_raise File.Error, "could not read file \"config/config.exs\": no such file or directory", fn ->
+      run([])
+    end
   end
 end
