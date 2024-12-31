3079517fc3d88adc7f2aebc8de5d56c13080313e
Gustavo Honorato <gustavohonorato@gmail.com>
2024-12-31 19:36:45 -0300

    Add one-time password generation function

--- lib/magic_auth.ex
+++ lib/magic_auth.ex
@@ -3,7 +3,78 @@ defmodule MagicAuth do
   Documentation for `MagicAuth`.
   """
 
+  import Ecto.Query
+  alias Ecto.Multi
+  alias Ecto.Changeset
+
   def one_time_password_length do
     Application.get_env(:magic_auth, :one_time_password_length, 6)
+  end
+
+  def one_time_password_expiration do
+    Application.get_env(:magic_auth, :one_time_password_expiration, 10)
+  end
+
+  defp repo do
+    Application.fetch_env!(:magic_auth, :repo)
+  end
+
+  @doc """
+  Generates a one-time password for a given email.
+
+  The token is stored in the database to allow users to log in from a device that
+  doesn't have access to the email where the code was sent. For example, the user
+  can receive the code on their phone and use it to log in on their computer.
+
+  ## Parameters
+
+    * `attrs` - A map containing `:email`
+
+  ## Returns
+
+    * `{:ok, token}` - Returns the created token on success
+    * `{:error, changeset}` - Returns the changeset with errors if validation fails
+    * `{:error, failed_value}` - Returns the failed value if the transaction fails
+
+  ## Examples
+
+      iex> MagicAuth.generate_one_time_password(%{"email" => "user@example.com", "value" => "123456"})
+      {:ok, %MagicAuth.Token{}}
+
+  The one time password length can be configured in config/config.exs:
+
+      config :magic_auth,
+        one_time_password_length: 6 # default value
+
+  This function:
+  1. Removes any existing tokens for the provided email
+  2. Generates a new random numeric password
+  3. Encrypts the password using Bcrypt
+  4. Stores the hash in the database
+  """
+  def generate_one_time_password(attrs) do
+    changeset = MagicAuth.Token.changeset(%MagicAuth.Token{}, attrs)
+
+    case changeset do
+      %Ecto.Changeset{valid?: true} = changeset ->
+        Multi.new()
+        |> Multi.delete_all(:delete_tokens, from(t in MagicAuth.Token, where: t.email == ^changeset.changes.email))
+        |> Multi.insert(:insert_token, fn _changes ->
+          one_time_password = Enum.map_join(1..MagicAuth.one_time_password_length(), fn _ -> Enum.random(0..9) end)
+          hashed_password = Bcrypt.hash_pwd_salt(one_time_password)
+          Changeset.put_change(changeset, :value, hashed_password)
+        end)
+        |> repo().transaction()
+        |> case do
+          {:ok, %{insert_token: token}} ->
+            {:ok, token}
+
+          {:error, _failed_operation, failed_value, _changes_so_far} ->
+            {:error, failed_value}
+        end
+
+      changeset ->
+        {:error, changeset}
+    end
   end
 end
--- test/magic_auth_test.exs
+++ test/magic_auth_test.exs
@@ -1,4 +1,66 @@
 defmodule MagicAuthTest do
-  use ExUnit.Case
-  doctest MagicAuth
+  use MagicAuth.DataCase, async: true
+  doctest MagicAuth, except: [generate_one_time_password: 1]
+
+  alias MagicAuth.Token
+
+  describe "one_time_password_length/0" do
+    test "returns default value when not configured" do
+      Application.delete_env(:magic_auth, :one_time_password_length)
+      assert MagicAuth.one_time_password_length() == 6
+    end
+
+    test "returns configured value" do
+      Application.put_env(:magic_auth, :one_time_password_length, 8)
+      assert MagicAuth.one_time_password_length() == 8
+      Application.delete_env(:magic_auth, :one_time_password_length)
+    end
+  end
+
+  describe "one_time_password_expiration/0" do
+    test "returns default value when not configured" do
+      Application.delete_env(:magic_auth, :one_time_password_expiration)
+      assert MagicAuth.one_time_password_expiration() == 10
+    end
+
+    test "returns configured value" do
+      Application.put_env(:magic_auth, :one_time_password_expiration, 15)
+      assert MagicAuth.one_time_password_expiration() == 15
+      Application.delete_env(:magic_auth, :one_time_password_expiration)
+    end
+  end
+
+  describe "generate_one_time_password/1" do
+    test "generates valid one-time password with valid email" do
+      email = "user@example.com"
+      {:ok, token} = MagicAuth.generate_one_time_password(%{"email" => email, "value" => "123456"})
+
+      assert token.email == email
+      assert String.length(token.value) > 0
+    end
+
+    test "returns error with invalid email" do
+      {:error, changeset} = MagicAuth.generate_one_time_password(%{"email" => "invalid_email", "value" => "123456"})
+      assert "has invalid format" in errors_on(changeset).email
+    end
+
+    test "removes existing tokens before creating a new one" do
+      email = "user@example.com"
+      {:ok, _token1} = MagicAuth.generate_one_time_password(%{"email" => email, "value" => "123456"})
+      {:ok, token2} = MagicAuth.generate_one_time_password(%{"email" => email, "value" => "654321"})
+
+      tokens = MagicAuth.TestRepo.all(Token)
+      assert length(tokens) == 1
+      assert List.first(tokens).id == token2.id
+    end
+
+    test "stores token value as bcrypt hash" do
+      {:ok, token} = MagicAuth.generate_one_time_password(%{"email" => "user@example.com", "value" => "123456test"})
+
+      # Verify value starts with "$2b$" which is the bcrypt hash identifier
+      assert String.starts_with?(token.value, "$2b$")
+      # Verify hash length is correct (60 characters)
+      assert String.length(token.value) == 60
+    end
+  end
 end
