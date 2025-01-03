9d5d07d4d4ad0281f58c3e2826c2d23c026d065e
Gustavo Honorato <gustavohonorato@gmail.com>
2025-01-03 15:21:11 -0300

    Implemented error paths of code verification WIP

--- lib/magic_auth/live/verify_live.ex
+++ lib/magic_auth/live/verify_live.ex
@@ -1,31 +1,55 @@
 defmodule MagicAuth.VerifyLive do
   use Phoenix.LiveView
-  alias Ecto.Changeset
+  alias MagicAuth.Session
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, form: changeset() |> to_form(as: "auth"))}
+    {:ok, assign(socket, form: to_password_form(nil), email: nil, error: nil)}
   end
+
+  defp to_password_form(password), do: to_form(%{"password" => password}, as: "auth")
 
   def handle_params(params, _uri, socket) do
-    {:noreply, assign(socket, email: params["email"])}
+    case parse_email(params) do
+      nil ->
+        {:noreply, push_navigate(socket, to: "/sessions/login")}
+
+      email ->
+        {:noreply, assign(socket, email: email, error: parse_error(params))}
+    end
   end
 
-  def handle_event("validate", attrs, socket) do
-    {:noreply, socket |> assign(form: changeset(attrs) |> to_form(as: "auth"))}
+  defp parse_email(%{"email" => email}) when is_binary(email) and byte_size(email) > 0 do
+    case String.match?(email, Session.email_pattern()) do
+      true -> email
+      false -> nil
+    end
   end
 
-  def handle_event("verify", attrs, socket) do
-    {:noreply, socket |> assign(form: changeset(attrs) |> to_form(as: "auth"))}
-  end
+  defp parse_email(_params), do: nil
 
-  defp changeset(attrs \\ %{}) do
-    types = %{code: :string}
+  defp parse_error(%{"error" => "invalid_code"}), do: "Invalid code"
+  defp parse_error(%{"error" => "code_expired"}), do: "Code expired"
+  defp parse_error(_params), do: nil
 
-    {%{}, types}
-    |> Changeset.cast(attrs, [:code])
-    |> Changeset.validate_required([:code])
-    |> Changeset.validate_length(:code, is: MagicAuth.Config.one_time_password_length())
-    |> Changeset.validate_format(:code, ~r/^[0-9]+$/, message: "deve conter apenas números")
+  def handle_event("verify", %{"auth" => %{"password" => password}}, socket) do
+    password_str = password |> Enum.join("") |> String.trim()
+    %{email: email} = socket.assigns
+
+    if String.length(password_str) == MagicAuth.Config.one_time_password_length() do
+      case MagicAuth.verify_password(email, password_str) do
+        {:error, :invalid_code} ->
+          {:noreply, assign(socket, error: "Invalid code", password: to_password_form(nil))}
+
+        {:error, :code_expired} ->
+          {:noreply, assign(socket, error: "Code expired", password: to_password_form(nil))}
+
+        {:ok, _session} ->
+          query = URI.encode_query(%{email: email})
+          {:noreply, push_navigate(socket, to: "/sessions/verify?#{query}")}
+      end
+    else
+      {:noreply, assign(socket, form: to_password_form(password), error: nil)}
+    end
   end
 
   def verify_form(assigns) do
@@ -35,7 +59,7 @@ defmodule MagicAuth.VerifyLive do
 
   def render(assigns) do
     ~H"""
-    <.verify_form form={@form} />
+    <.verify_form form={@form} email={@email} error={@error} />
     """
   end
 end
